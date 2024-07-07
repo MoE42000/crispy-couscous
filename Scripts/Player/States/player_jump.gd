7a442dce8c3161ab297efe949d7b66afe5c4e372
Mauro <114999241+mauro-vb@users.noreply.github.com>
2024-07-07 13:03:09 +0200

    added pushback, changing the movement variable in teh jumop state, needs fixing

--- Scripts/Player/States/player_jump.gd
+++ Scripts/Player/States/player_jump.gd
@@ -1,11 +1,12 @@
 extends PlayerState
 class_name PlayerJump
 
-@export var wall_jump_pushback : float
+var wall_jump_pushback : float
+var dir : float
 
 func enter() -> void:
 	super()
-	
+	dir = character.direction
 	if character.movement.get_up_down_input() > 0:
 		if !character.drop_through():
 			jump()
@@ -18,7 +19,12 @@ func jump():
 	#TODO: Come up with personalized Jump func
 	character.velocity.y = character.jump_force
 	if previous_state is PlayerSlide:
-		character.velocity.x = wall_jump_pushback * character.direction
+		# Apply pushback in the opposite direction of the wall
+		wall_jump_pushback = 200.0 # Example value, set this to your desired pushback forc e
+		# Reverse direction if wall sliding
+		character.velocity.x = wall_jump_pushback * -dir
+	else:
+		wall_jump_pushback = 0
 	
 	
 	
@@ -30,14 +36,16 @@ func process_physics(delta):
 		
 	if character.movement.wants_attack():
 		transitioned.emit(self,'attack')
-
-	var movement = character.direction * character.speed 
+		
+	wall_jump_pushback -= delta * wall_jump_pushback
+	var movement = character.direction * character.speed
+	if wall_jump_pushback > 0:
+		movement += wall_jump_pushback * -dir
 	character.velocity.x = movement
 	
 	
 	if character.velocity.y > 0:
 		transitioned.emit(self, "fall") 
-	
 
 	super(delta)
 	#character.move_and_slide()
