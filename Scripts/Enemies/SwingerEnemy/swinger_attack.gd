20f793c91a972f6e4d35698c0ed4b99ca524a7ae
Mauro <mvb.codes@gmail.com>
2024-07-24 19:25:49 +0200

    improved wall shooting spikes

--- Scripts/Enemies/SwingerEnemy/swinger_attack.gd
+++ Scripts/Enemies/SwingerEnemy/swinger_attack.gd
@@ -1,13 +1,15 @@
 extends State
 
 func enter() -> void:
-	change_animation(animation_name)
+	
 	character.handle_flipping(character.position.direction_to(Global.player.position).x)
 		
 	character.velocity = Vector2(0,0) # stands still
 	var animation_tree = get_parent().animation_tree
 	if !animation_tree.animation_finished.is_connected(attack_finished):
 		animation_tree.animation_finished.connect(attack_finished)
+	await get_tree().create_timer(.1).timeout
+	change_animation(animation_name)
 
 
 func attack_finished(_animation):
--- Scripts/Environment/Hazards/spike_projectile_shooter.gd
+++ Scripts/Environment/Hazards/spike_projectile_shooter.gd
@@ -6,16 +6,24 @@ var timer : Timer
 
 @export
 var shoot_rate : float = 1.0
+@export 
+var start_shooting_offset : float = 0
 @export
 var projectile_speed : float = 200
 
 
+
+@onready 
+var animation_player := $AnimationPlayer
+
 func _ready():
 	timer = Timer.new()
-	timer.autostart = true
+	
 	timer.timeout.connect(_on_timer_timeout)
 	timer.wait_time = shoot_rate
 	add_child(timer)
+	await get_tree().create_timer(start_shooting_offset).timeout
+	timer.start()
 	
 	
 
@@ -25,7 +33,10 @@ func shoot():
 	instance.spawn_pos = Vector2(global_position.x, global_position.y)
 	instance.spawn_rotation = rotation
 	instance.speed = projectile_speed
+	
 	add_child(instance)
 
 func _on_timer_timeout():
+	animation_player.play("shoot")
+	await get_tree().create_timer(.5).timeout
 	shoot()
