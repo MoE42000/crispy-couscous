2e08e122c6371ebd1be082da9c133dac323c2938
Mauro <mvb.codes@gmail.com>
2024-07-26 17:37:02 +0200

    added multiplicative speed change and direction cahnges to moving hazard

--- Scripts/Environment/Hazards/moveable_hazard.gd
+++ Scripts/Environment/Hazards/moveable_hazard.gd
@@ -2,22 +2,25 @@
 extends CharacterBody2D
 class_name BouncingHazard
 
-@export var movement_type : MOVEMENT_TYPES = MOVEMENT_TYPES.Default :
+@export var initial_dir : Vector2 = Vector2.ZERO
+@export var initial_speed : float = 50
+@export var damage_ammount : float = 10
+
+@export var speed_change_type : SPEED_CHANGE_TYPES = SPEED_CHANGE_TYPES.None :
 	set(value):
-		movement_type = value
+		speed_change_type = value
 		notify_property_list_changed()
 
-enum MOVEMENT_TYPES {
-	Default,
-	Increasing,
+enum SPEED_CHANGE_TYPES {
+	None,
+	Linear,
+	Multiplicative,
 	Discrete,
 	Random,
 }
 
 var speed_change : float
 var max_speed : float = 1000
-var increase_type : INCREASE_TYPE = INCREASE_TYPE.Linear
-enum INCREASE_TYPE {Linear,Multiplicative}
 var change_on_bounce : bool = true :
 	set(value):
 		change_on_bounce = value
@@ -26,18 +29,56 @@ var change_time_rate : float
 var speed_values : Array[float]
 var speed_value_index : int = 0
 var speed_range : Vector2
+	
+@export var direction_change_type : DIRECTION_CHANGE_TYPES = DIRECTION_CHANGE_TYPES.None :
+	set(value):
+		direction_change_type = value
+		notify_property_list_changed()
+
+enum DIRECTION_CHANGE_TYPES {
+	None,
+	X,
+	Y,
+}
+
+var change_direction_time_rate : float
 
 func _get_property_list() -> Array[Dictionary]:
 	var properties : Array[Dictionary] = []
-	match movement_type:
-		MOVEMENT_TYPES.Increasing:
+	
+	match speed_change_type:
+		SPEED_CHANGE_TYPES.Linear:
 			properties.append({
-				"name":"increase_type",
-				"type":TYPE_INT,
+				"name":"speed_change",
+				"type":TYPE_FLOAT,
 				"usage":PROPERTY_USAGE_DEFAULT,
 				"hint":PROPERTY_HINT_ENUM,
 				"hint_string":""
 			})
+			properties.append({
+				"name":"max_speed",
+				"type":TYPE_FLOAT,
+				"usage":PROPERTY_USAGE_DEFAULT,
+				"hint":PROPERTY_HINT_ENUM,
+				"hint_string":""
+			})
+			properties.append({
+				"name":"change_on_bounce",
+				"type":TYPE_BOOL,
+				"usage":PROPERTY_USAGE_DEFAULT,
+				"hint":PROPERTY_HINT_ENUM,
+				"hint_string":""
+			})
+			if !change_on_bounce:
+				properties.append({
+					"name":"change_time_rate",
+					"type":TYPE_FLOAT,
+					"usage":PROPERTY_USAGE_DEFAULT,
+					"hint":PROPERTY_HINT_ENUM,
+					"hint_string":""
+				})
+		
+		SPEED_CHANGE_TYPES.Multiplicative:
 			properties.append({
 				"name":"speed_change",
 				"type":TYPE_FLOAT,
@@ -68,7 +109,7 @@ func _get_property_list() -> Array[Dictionary]:
 					"hint_string":""
 				})
 
-		MOVEMENT_TYPES.Discrete:
+		SPEED_CHANGE_TYPES.Discrete:
 			properties.append({
 				"name":"speed_values",
 				"type":TYPE_ARRAY,
@@ -92,7 +133,7 @@ func _get_property_list() -> Array[Dictionary]:
 					"hint_string":""
 				})
 			
-		MOVEMENT_TYPES.Random:
+		SPEED_CHANGE_TYPES.Random:
 			properties.append({
 				"name":"speed_range",
 				"type":TYPE_VECTOR2,
@@ -117,14 +158,21 @@ func _get_property_list() -> Array[Dictionary]:
 				})
 		_ :
 			properties
+	
+	if direction_change_type != DIRECTION_CHANGE_TYPES.None:
+		properties.append({
+			"name":"change_direction_time_rate",
+			"type":TYPE_FLOAT,
+			"usage":PROPERTY_USAGE_DEFAULT,
+			"hint":PROPERTY_HINT_ENUM,
+			"hint_string":""
+		})
+	
 	return properties
-
-@export var initial_dir : Vector2 = Vector2.ZERO
-@export var initial_speed : float = 50
-@export var damage_ammount : float = 10
 
 var speed : float
 var speed_timer : Timer
+var direction_timer : Timer
 
 
 
@@ -138,7 +186,13 @@ func _ready():
 		speed_timer.timeout.connect(_on_speed_timer_timeout)
 		speed_timer.start()
 	
-	
+	if direction_change_type != DIRECTION_CHANGE_TYPES.None:
+		direction_timer = Timer.new()
+		direction_timer.wait_time = change_direction_time_rate
+		add_child(direction_timer)
+		direction_timer.timeout.connect(_on_direction_timer_timeout)
+		direction_timer.start()
+		
 	velocity = initial_dir.normalized() * initial_speed
 	hurt_area.body_entered.connect(_on_body_entered)
 
@@ -152,17 +206,19 @@ func _physics_process(delta: float) -> void:
 
 
 func update_speed():
-	match movement_type:
-		MOVEMENT_TYPES.Increasing:
-			speed = min(speed + speed_change, max_speed) if INCREASE_TYPE.Linear else min(speed * speed_change, max_speed)
+	match speed_change_type:
+		SPEED_CHANGE_TYPES.Linear:
+			speed = min(speed + speed_change, max_speed)
 			
-		MOVEMENT_TYPES.Discrete:
+		SPEED_CHANGE_TYPES.Multiplicative:
+			speed = min(speed * speed_change, max_speed)
+			
+		SPEED_CHANGE_TYPES.Discrete:
 			speed = speed_values[speed_value_index]
 			speed_value_index = (speed_value_index + 1) % speed_values.size() 
 			
-		MOVEMENT_TYPES.Random:
+		SPEED_CHANGE_TYPES.Random:
 			speed = randf_range(speed_range.x, speed_range.y)
-		
 		_:
 			pass
 	
@@ -170,6 +226,18 @@ func update_speed():
 			
 func _on_speed_timer_timeout():
 	update_speed()
+	
+func update_direction():
+	match direction_change_type:
+		DIRECTION_CHANGE_TYPES.X:
+			velocity.x *= -1
+		DIRECTION_CHANGE_TYPES.Y:
+			velocity.y *= -1
+		_:
+			pass
+			
+func _on_direction_timer_timeout():
+	update_direction()
 	
 	
 	
