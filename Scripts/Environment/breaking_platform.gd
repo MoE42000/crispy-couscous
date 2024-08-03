9ca4cbf38fdbd3f9e83c3548f0bca39af6215a6d
Mauro <mvb.codes@gmail.com>
2024-08-03 16:34:58 +0200

    added reconstruction to fragile platform

--- Scripts/Environment/breaking_platform.gd
+++ Scripts/Environment/breaking_platform.gd
@@ -2,12 +2,14 @@ extends Area2D
 class_name BreakingPlatform
 
 @export var resets : bool = true
+@export var reconstruct_time : float = 3
 @onready var animation := $AnimationPlayer
 
 func _ready():
 	body_entered.connect(_on_body_entered)
 	
 func _on_body_entered(_body):
+	await get_tree().create_timer(0.2).timeout
 	animation.play("break")
-	#await animation.animation_finished
-	#queue_free()
+	await get_tree().create_timer(reconstruct_time).timeout
+	animation.play("reconstruct")
